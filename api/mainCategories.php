6a41a68f7271e2111854000070bc13bbbd540834
wndeepak31 <dnishad031@gmail.com>
2021-11-08 15:35:44 +0530

    api files

---
+++ api/mainCategories.php
@@ -0,0 +1,16 @@
+<?php
+
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+
+include "../config/connection.php";
+
+$sql = "SELECT * FROM categories WHERE  Publish='Yes'";
+$result = mysqli_query($link, $sql) or die("SQL Query Failed.");
+
+if (mysqli_num_rows($result) > 0) {
+	$output = mysqli_fetch_all($result, MYSQLI_ASSOC);
+	echo json_encode($output);
+} else {
+	echo json_encode(array('message' => 'Data Not Found'));
+}
